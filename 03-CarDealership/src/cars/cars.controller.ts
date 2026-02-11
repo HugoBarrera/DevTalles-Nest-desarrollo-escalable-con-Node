f7775a10ec988ff52230c815712a352f8e5b8a6a
Hugo Barrera <exailer89@gmail.com>
2026-02-11 00:06:56 -0600

    Sección 04: ValidationPipe - Class Validator y Class Transformer

--- 03-CarDealership/src/cars/cars.controller.ts
+++ 03-CarDealership/src/cars/cars.controller.ts
@@ -15,7 +15,7 @@ import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
-@UsePipes(ValidationPipe) // Este Pipe esta disponible para todos los metodos del controlador
+// @UsePipes(ValidationPipe) // Este Pipe esta disponible para todos los metodos del controlador
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
